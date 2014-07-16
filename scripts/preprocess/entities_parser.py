1fc9e52ef769d68f7b9742325a39aef548ac8e8b
nacho cano <nachoacano@gmail.com>
2014-07-16 16:24:47 -0700

    updating parse to target entities. There are 109. Only 30 contain surface names. May need to query different APIs to get more recall

--- scripts/preprocess/entities_parser.py
+++ scripts/preprocess/entities_parser.py
@@ -1,49 +1,50 @@
 import argparse
 import yaml
 
-parser = argparse.ArgumentParser()
-parser.add_argument('input')
-args = parser.parse_args()
-
-profiles = yaml.load(open(args.input))
-total_entities = 0
-entities_with_canonical = 0
-entities_with_name = 0
-for entity in profiles['entities']:
-	total_entities += 1
-	slots = entity.get('slots', [])
-	target_canonical_name = ''
+def extract_name(name):
 	target_surface_forms = ''
-	has_name = False
-	has_canonical = False
-	for slot in slots:
-		if (slot.get('canonical_name')):
-			entities_with_canonical +=1
-			has_canonical = True
-			canonical_name = slot.get('canonical_name')
-			if type(canonical_name) is str:
-				target_canonical_name = canonical_name.strip().lower()
-			elif type(canonical_name) is dict:
-				target_canonical_name = canonical_name.get('value').strip().lower()
-		elif (slot.get('NAME')):
-			entities_with_name += 1
-			has_name = True
-			name = slot.get('NAME')
-			if type(name) is str:
-				target_surface_forms += name.strip().lower()
-			elif type(name) is dict:
-				target_surface_forms += name.get('value').strip().lower()
-			elif type(name) is list:
-				aux = []
-				for e in name:
-					if type(e) is str:
-						aux.append(e.strip().lower())
-					elif type(e) is dict:
-						aux.append(e.get('value').strip().lower())
-				target_surface_forms += ",".join(aux)
-	if has_canonical and has_name:
-		print '%s|%s' % (target_canonical_name, target_surface_forms)
-	elif has_canonical:
-		print '%s' % target_canonical_name
+	if type(name) is str:
+		target_surface_forms += name.strip().lower()
+	elif type(name) is dict:
+		target_surface_forms += name.get('value').strip().lower()
+	elif type(name) is list:
+		aux = []
+		for e in name:
+			if type(e) is str:
+				aux.append(e.strip().lower())
+			elif type(e) is dict:
+				aux.append(e.get('value').strip().lower())
+		target_surface_forms += ",".join(aux)
+	return target_surface_forms
 
-#print '\nstats: total=%d, canonical=%d, name=%d' % (total_entities, entities_with_canonical, entities_with_name) 
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('input')
+	args = parser.parse_args()
+
+	profiles = yaml.load(open(args.input))
+	total_canonical = 0
+	total_surface = 0
+
+	for label, entity in profiles['entities'].items():
+		has_surface = False
+		canonical = ''
+		surface = ''
+		for slot_name, slot_value in entity['slots'].items():
+			if slot_name == 'canonical_name':
+				total_canonical += 1
+				canonical = slot_value.strip().lower()
+			if slot_name == 'PER_NAME' or slot_name == 'ORG_NAME' or slot_name == 'FAC_NAME':
+				total_surface += 1
+				has_surface = True
+				surface = extract_name(slot_value)
+		if has_surface:
+			print '%s|%s' % (canonical, surface)
+		else:
+			print '%s' % canonical
+
+	#print '\nstats: canonical=%d, surface=%d' % (total_canonical, total_surface)
+
+if __name__ == '__main__':
+	main()
