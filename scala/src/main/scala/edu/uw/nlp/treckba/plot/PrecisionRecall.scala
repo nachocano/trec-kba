8a49740b439345a0a6d06fa73f8270bba7e88278
nacho cano <nachoacano@gmail.com>
2014-08-02 23:52:29 -0700

    adding scalaplot and precision recall script

---
+++ scala/src/main/scala/edu/uw/nlp/treckba/plot/PrecisionRecall.scala
@@ -0,0 +1,57 @@
+package edu.uw.nlp.treckba.plot
+
+import org.apache.commons.cli._
+import org.sameersingh.scalaplot.gnuplot.GnuplotPlotter
+import scala.collection.mutable.ArrayBuffer
+import scala.io.Source
+import org.apache.commons.lang.Validate
+import org.sameersingh.scalaplot.metrics.PrecRecallCurve
+
+object PrecisionRecall {
+
+  def main(args: Array[String]) {
+    val options = new Options()
+    options.addOption("i", true, "input file")
+    options.addOption("o", true, "output directory")
+    options.addOption("f", true, "output filename")
+
+    val parser = new BasicParser()
+
+    var inputFile: String = null
+    var outputDir: String = null
+    var outFilename: String = null
+    try {
+      val line = parser.parse(options, args)
+      inputFile = line.getOptionValue("i")
+      Validate.notNull(inputFile)
+      outputDir = line.getOptionValue("o")
+      Validate.notNull(outputDir)
+      outFilename = line.getOptionValue("f")
+      Validate.notNull(outFilename)
+
+    } catch {
+      case ex: Exception => {
+        val formatter = new HelpFormatter()
+        formatter.printHelp("precisionrecall", options)
+        return
+      }
+    }
+
+    val list = new ArrayBuffer[(Double,Boolean)]
+    for (line <- Source.fromFile(inputFile).getLines) {
+      val values = line.split(" ")
+      val prob = values(0).toDouble
+      var truth = false
+      if (values(1).toInt == 1)
+        truth = true
+      val tuple = (prob, truth)
+      list += tuple
+    }
+
+    val curve = new PrecRecallCurve(list)
+    val chart = curve.prChart("Precision-Recall")
+    val plotter = new GnuplotPlotter(chart)
+    plotter.pdf(outputDir, outFilename)
+
+  }
+}
---
+++ scripts/precision_recall.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+from __future__ import division
+import argparse
+from collections import defaultdict
+
+
+def main():
+ 
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-r', '--run_file', required=True)
+    parser.add_argument('-gt', '--ground_truth_file', required=True)
+    parser.add_argument('-e', '--entities_ccr', required=True)
+    parser.add_argument('-m', '--mode', required=True)
+    args = parser.parse_args()
+
+    assert(args.mode == 'uv' or args.mode == 'rnr')
+
+    targetids = {}
+    for line in open(args.entities_ccr).read().splitlines():
+        targetids[line.strip()] = True
+
+    ground_truth = {}
+    with open(args.ground_truth_file) as f:
+        for line in f.read().splitlines():
+            l = line.strip()
+            if l.startswith('#'):
+                continue
+            l = l.split('\t')
+            targetid = l[3]
+            if targetids.has_key(targetid):
+                streamid = l[2]
+                date_hour = l[7]
+                candidate_relevance = int(l[5])
+                if args.mode == 'rnr':
+                    relevance = 1 if (candidate_relevance == 1 or candidate_relevance == 2) else 0
+                else:
+                    if candidate_relevance == 0 or candidate_relevance == -1:
+                        continue
+                    relevance = 1 if candidate_relevance == 2 else 0
+                ground_truth[(streamid, targetid, date_hour)] = relevance
+
+    run = {}
+    with open(args.run_file) as f:
+        for line in f.read().splitlines():
+            l = line.strip()
+            if l.startswith('#'):
+                continue
+            l = l.split('\t')
+            streamid = l[2]
+            targetid = l[3]
+            relevance = int(l[5])
+            date_hour = l[7]
+            probability = int(l[4]) / 1000
+            run[(streamid, targetid, date_hour)] = (relevance, probability)
+
+    for key in ground_truth:
+        targetid = key[1]
+        if run.has_key(key):
+            print '%s %s' % (run[key][1], ground_truth[key])
+
+
+if __name__ == '__main__':
+  main()
