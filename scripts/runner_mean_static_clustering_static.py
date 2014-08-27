dbdd07903d60d1d025692385f73de51995f35ce7
nacho cano <nachoacano@gmail.com>
2014-08-26 17:17:58 -0700

    adding some runners

---
+++ scripts/runner_mean_static_clustering_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+
+def main():
+
+  mean_static = 'nohup ../../../../py_virtual/bin/python -u gbt_clustering.py -e ../../../clustering/data/trec-kba-2014-07-11-ccr-and-ssf-query-topics.json \
+                  -o ../../../clustering/data/outputs/uw-mean_static.txt -tr ../../../clustering/data/train_sorted.tsv -t ../../../clustering/data/test_sorted.tsv \
+                  -i uw-mean_static -av 1 -an 1 -gvi 1 -gvd 1 -gni 1 -gnd 1 -s 0 -c ../../../clustering/ -rnrl ../../../clustering/model/rnr.pkl > ../../../clustering/logs/uw-mean_static.log'
+  print mean_static
+  os.system(mean_static)
+
+  alphas = [0.2,0.4,0.6,0.8]
+  alphas_name = ['02','04','06','08']
+  for alpha, name in zip(alphas, alphas_name):
+    clustering_static = 'nohup ../../../../py_virtual/bin/python -u gbt_clustering.py -e ../../../clustering/data/trec-kba-2014-07-11-ccr-and-ssf-query-topics.json \
+                  -o ../../../clustering/data/outputs/uw-clustering_static_a%s.txt -tr ../../../clustering/data/train_sorted.tsv -t ../../../clustering/data/test_sorted.tsv \
+                  -i uw-clustering_static_a%s -av %s -an %s -gvi 1 -gvd 1 -gni 1 -gnd 1 -s 0 -c ../../../clustering/ -rnrl ../../../clustering/model/rnr.pkl \
+                  > ../../../clustering/logs/uw-clustering_static_a%s.log' % (name, name, alpha, alpha, name)
+  print clustering_static
+  os.system(clustering_static)
+
+if __name__ == '__main__':
+  main()
