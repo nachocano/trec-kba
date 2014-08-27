600cd42d185f3344b0668d66a18443bbb0941fea
nacho cano <nachoacano@gmail.com>
2014-08-26 17:52:55 -0700

    and

--- scripts/runner_mean_dynamic.py
+++ scripts/runner_mean_dynamic.py
@@ -5,10 +5,10 @@ def main():
   gammas = [0.2,0.4,0.6,0.8]
   gammas_name = ['02','04','06','08']
   for gamma, name in zip(gammas, gammas_name):
-    mean_dynamic = 'nohup ../../../../py_virtual/bin/python -u gbt_clustering.py -e ../../../clustering/data/trec-kba-2014-07-11-ccr-and-ssf-query-topics.json' \
-                  ' -o ../../../clustering/data/outputs/uw-mean_dynamic_g%s.txt -tr ../../../clustering/data/train_sorted.tsv -t ../../../clustering/data/test_sorted.tsv' \
-                  ' -i uw-mean_dynamic_g%s -av 1 -an 1 -gvi %s -gvd %s -gni %s -gnd %s -s 0 -c ../../../clustering/ -rnrl ../../../clustering/model/rnr.pkl' \
-                  ' > ../../../clustering/logs/uw-mean_dynamic_g%s.log' % (name, name, gamma, gamma, gamma, gamma, name)
+    mean_dynamic = 'nohup ../../../../py_virtual/bin/python -u gbt_clustering.py -e ../../../clustering/data/trec-kba-2014-07-11-ccr-and-ssf-query-topics.json \
+                  -o ../../../clustering/data/outputs/uw-mean_dynamic_g%s.txt -tr ../../../clustering/data/train_sorted.tsv -t ../../../clustering/data/test_sorted.tsv \
+                  -i uw-mean_dynamic_g%s -av 1 -an 1 -gvi %s -gvd %s -gni %s -gnd %s -s 0 -c ../../../clustering/ -rnrl ../../../clustering/model/rnr.pkl \
+                  > ../../../clustering/logs/uw-mean_dynamic_g%s.log' % (name, name, gamma, gamma, gamma, gamma, name)
     print mean_dynamic
     os.system(mean_dynamic)
 
