db375d24b96209f41d48ad187ee431e25d5412bc
nacho cano <nachoacano@gmail.com>
2014-09-06 01:54:22 -0700

    updating runner clu dyn

--- scripts/runner_clustering_dynamic.py
+++ scripts/runner_clustering_dynamic.py
@@ -3,19 +3,18 @@ import os
 
 def main():
 
-  alphas = [0.2,0.6]
-  alphas_name = ['02', '06']
-  gammas = [0.4,0.8]
-  gammas_name = ['04', '08']
+  gammas_name = ['02', '04', '06','08','1','2','5','10','20']
   
-  for alpha, alpha_name in zip(alphas, alphas_name):
-    for gamma, gamma_name in zip(gammas, gammas_name):
-      clustering_dynamic = 'nohup ../../../../py_virtual/bin/python -u gbt_clustering.py -e ../../../clustering/data/trec-kba-2014-07-11-ccr-and-ssf-query-topics.json \
-                  -o ../../../clustering/outputs/uw-clustering_dynamic_a%s_g%s.txt -tr ../../../clustering/data/train_sorted.tsv -t ../../../clustering/data/test_sorted.tsv \
-                  -i uw-clustering_dynamic_a%s_g%s -av %s -an %s -gvi %s -gvd %s -gni %s -gnd %s -s 0 -c ../../../clustering/ -rnrl ../../../clustering/model/rnr.pkl \
-                  > ../../../clustering/logs/uw-clustering_dynamic_a%s_g%s.log' % (alpha_name, gamma_name, alpha_name, gamma_name, alpha, alpha, gamma, gamma, gamma, gamma, alpha_name, gamma_name)
-      print clustering_dynamic
-      os.system(clustering_dynamic)
+  for gamma_name in gammas_name:
+    clu_dyn = 'nohup ../../../../py_virtual/bin/python -u clustering_uv.py -e ../../../clustering/misc/trec-kba-2014-07-11-ccr-and-ssf-query-topics.json \
+                -tr ../../../clustering/outputs/train_r_clu_dyn_a08_gd%s_gi04_exp_global.tsv \
+                -t ../../../clustering/outputs/test_r_clu_dyn_a08_gd%s_gi04_exp_global.tsv \
+                -i clu_dyn_a08_gd%s_gi04_exp_global -rnrl ../../../clustering/model/rnr.pkl \
+                -nr ../../../clustering/misc/nr.txt \
+                -o ../../../clustering/outputs/uw-clu_dyn_a08_gd%s_gi04 \
+                > ../../../clustering/logs/uw-clu_dyn_a08_gd%s_gi04_exp_global.log' % (gamma_name, gamma_name, gamma_name, gamma_name, gamma_name)
+    print clu_dyn
+    os.system(clu_dyn)
 
 if __name__ == '__main__':
   main()
