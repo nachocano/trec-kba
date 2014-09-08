ff90ed1718507b1d089543bc349d9daa1d776933
nacho cano <nachoacano@gmail.com>
2014-09-08 16:46:56 -0700

    adding debugging in java

---
+++ java/src/main/java/edu/uw/nlp/treckba/debugging/Debugging.java
@@ -0,0 +1,77 @@
+package edu.uw.nlp.treckba.debugging;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.transport.TIOStreamTransport;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
+import edu.uw.nlp.treckba.gen.Sentence;
+import edu.uw.nlp.treckba.gen.StreamItem;
+import edu.uw.nlp.treckba.gen.Token;
+import edu.uw.nlp.treckba.utils.Utils;
+
+public class Debugging {
+
+	public void debug(final String inputFile, final String outputFile,
+			final List<String> streamIds, final List<String> entityNames) {
+		final String fullMatch = Utils.fullMatch(entityNames);
+		final Pattern full = Pattern.compile(fullMatch);
+		TTransport transport = null;
+		PrintWriter pw = null;
+		try {
+			pw = new PrintWriter(new File(outputFile));
+			transport = new TIOStreamTransport(new BufferedInputStream(
+					new FileInputStream(inputFile)));
+			final TBinaryProtocol protocol = new TBinaryProtocol(transport);
+			transport.open();
+			while (true) {
+				final StreamItem item = new StreamItem();
+				item.read(protocol);
+				final String streamId = item.getStream_id();
+				if (streamIds.contains(streamId)) {
+					final List<Sentence> sentences = item.getBody()
+							.getSentences().get("serif");
+					final List<String> lemmasForStreamId = new ArrayList<>();
+					for (final Sentence sentence : sentences) {
+						final List<Token> tokens = sentence.getTokens();
+						final StringBuilder lemmas = new StringBuilder();
+						for (final Token token : tokens) {
+							final String lemma = token.getLemma();
+							lemmas.append(lemma).append(" ");
+						}
+						if (full.matcher(lemmas.toString()).find()) {
+							lemmasForStreamId.add(lemmas.toString());
+						}
+					}
+					pw.println(String.format("%s %s", streamId,
+							Arrays.toString(lemmasForStreamId.toArray())));
+				}
+			}
+		} catch (final TTransportException te) {
+			if (te.getType() != TTransportException.END_OF_FILE) {
+				System.err.println(String.format(
+						"exception with file %s. exc %s", inputFile,
+						te.getMessage()));
+			}
+		} catch (final Exception exc) {
+			System.err
+					.println(String.format("exception: %s", exc.getMessage()));
+		} finally {
+			if (transport != null) {
+				transport.close();
+			}
+			if (pw != null) {
+				pw.close();
+			}
+		}
+	}
+}
---
+++ java/src/main/java/edu/uw/nlp/treckba/debugging/DebuggingDriver.java
@@ -0,0 +1,64 @@
+package edu.uw.nlp.treckba.debugging;
+
+import java.util.List;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.lang3.Validate;
+
+import edu.uw.nlp.treckba.utils.Utils;
+
+public class DebuggingDriver {
+
+	public static void main(final String[] args) throws Exception {
+		final Options options = new Options();
+		options.addOption("i", true, "input entity bin file");
+		options.addOption("fp", true, "false positives entity file");
+		options.addOption("o", true, "output file");
+		options.addOption("e", true, "entitites file with surface forms");
+
+		final CommandLineParser parser = new BasicParser();
+
+		String inputEntityBinFile = null;
+		String falsePositiveFile = null;
+		String outputFile = null;
+		String entitiesFile = null;
+		try {
+			final CommandLine line = parser.parse(options, args);
+			inputEntityBinFile = line.getOptionValue("i");
+			Validate.notNull(inputEntityBinFile);
+			falsePositiveFile = line.getOptionValue("fp");
+			Validate.notNull(falsePositiveFile);
+			outputFile = line.getOptionValue("o");
+			Validate.notNull(outputFile);
+			entitiesFile = line.getOptionValue("e");
+			Validate.notNull(entitiesFile);
+
+		} catch (final Exception e) {
+			final HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("debugging", options);
+			return;
+		}
+
+		final long start = System.currentTimeMillis();
+		final List<String> streamIds = Utils
+				.readFalsePositives(falsePositiveFile);
+		final String filename = inputEntityBinFile.substring(
+				inputEntityBinFile.lastIndexOf("/") + 1,
+				inputEntityBinFile.length());
+		final String targetId = Utils.DIFFEO_URL + filename.replace(".bin", "");
+		System.out.println(targetId);
+		final List<String> entityNames = Utils.readFile(entitiesFile).get(
+				targetId);
+
+		final Debugging debugging = new Debugging();
+
+		debugging.debug(inputEntityBinFile, outputFile, streamIds, entityNames);
+
+		System.out.println(String.format("total time during processing %s",
+				(System.currentTimeMillis() - start) / 1000));
+	}
+}
