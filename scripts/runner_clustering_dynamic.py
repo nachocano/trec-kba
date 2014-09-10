96a3e64f94f8c116d2a0805ea4c8cf05b9ffe0c3
nacho cano <nachoacano@gmail.com>
2014-09-10 01:59:30 -0700

    updating runners

--- scripts/runner_clustering_dynamic.py
+++ scripts/runner_clustering_dynamic.py
@@ -3,20 +3,18 @@ import os
 
 def main():
 
-  gammas_increase = [0.1, 0.5, 0.9]
-  gammas_decrease = [100, 200, 500]
-  gammas_increase_name = ['01', '05', '09']
-  gammas_decrease_name = ['100', '200', '500']
+  gammas_increase = [(0.1, '01'), (0.5, '05'), (0.9, '09')]
+  gammas_decrease = [(0.1, '01'), (0.5, '0.5'), (1, '1'), (10, '10'), (20, '20'), (50, '50'), (100, '100'), (200, '200')]
 
-  for gamma_decrease, gamma_decrease_name in zip(gammas_decrease, gammas_decrease_name):
-    for gamma_increase, gamma_increase_name in zip(gammas_increase, gammas_increase_name):
-      clu_dyn = 'nohup ../../../../py_virtual/bin/python -u clustering_uv.py -e ../../../clustering/misc/trec-kba-2014-07-11-ccr-and-ssf-query-topics.json \
-                -tr ../../../clustering/outputs/train_r_clu_dyn_a08_gd%s_gi%s_exp_global.tsv \
-                -t ../../../clustering/outputs/test_r_clu_dyn_a08_gd%s_gi%s_exp_global.tsv \
-                -i clu_dyn_a08_gd%s_gi%s_exp_global -rnrl ../../../clustering/model/rnr.pkl \
-                -nr ../../../clustering/misc/nr.txt \
-                -o ../../../clustering/outputs/uw-clu_dyn_a08_gd%s_gi%s \
-                > ../../../clustering/logs/uw-clu_dyn_a08_gd%s_gi%s_exp_global.log' % (gamma_decrease_name, gamma_increase_name, gamma_decrease_name, gamma_increase_name, gamma_decrease_name, gamma_increase_name, gamma_decrease_name, gamma_increase_name, gamma_decrease_name, gamma_increase_name)
+  for gamma_decrease, gamma_decrease_name in gammas_decrease:
+    for gamma_increase, gamma_increase_name in gammas_increase:
+      clu_dyn = 'nohup ../../../../py_virtual/bin/python -u clustering_uv.py -e ../../../last_clustering/misc/trec-kba-2014-07-11-ccr-and-ssf-query-topics.json \
+                -tr ../../../last_clustering/outputs/train_r_clu_dyn_a08_gd%s_gi%s_p.tsv \
+                -t ../../../last_clustering/outputs/test_r_clu_dyn_a08_gd%s_gi%s_p.tsv \
+                -i clu_dyn_a08_gd%s_gi%s_p -rnrl ../../../last_clustering/model/rnr.pkl \
+                -nr ../../../last_clustering/misc/nr.txt \
+                -o ../../../last_clustering/outputs/uw-clu_dyn_a08_gd%s_gi%s_p \
+                > ../../../last_clustering/logs/uw-clu_dyn_a08_gd%s_gi%s_p.log' % (gamma_decrease_name, gamma_increase_name, gamma_decrease_name, gamma_increase_name, gamma_decrease_name, gamma_increase_name, gamma_decrease_name, gamma_increase_name, gamma_decrease_name, gamma_increase_name)
       print clu_dyn
       os.system(clu_dyn)
 
--- scripts/runner_feature_generator.py
+++ scripts/runner_feature_generator.py
@@ -3,18 +3,16 @@ import os
 
 def main():
 
-  gammas_increase = [0.1, 0.5, 0.9]
-  gammas_decrease = [100, 200, 500]
-  gammas_increase_name = ['01', '05', '09']
-  gammas_decrease_name = ['100', '200', '500']
+  gammas_increase = [(0.1, '01'), (0.5, '05'), (0.9, '09')]
+  gammas_decrease = [(0.1, '01'), (0.5, '0.5'), (1, '1'), (10, '10'), (20, '20'), (50, '50'), (100, '100'), (200, '200')]
 
-  for gamma_decrease, gamma_decrease_name in zip(gammas_decrease, gammas_decrease_name):
-    for gamma_increase, gamma_increase_name in zip(gammas_increase, gammas_increase_name):
+  for gamma_decrease, gamma_decrease_name in gammas_decrease:
+    for gamma_increase, gamma_increase_name in gammas_increase:
       cmd = 'nohup java -jar treckba-jar-with-dependencies.jar -tr ../data/test_r.tsv \
-                -trr ../data/train_r.tsv -ot ../outputs/test_r_clu_dyn_a08_gd%s_gi%s_exp_global.tsv \
-                -otr ../outputs/train_r_clu_dyn_a08_gd%s_gi%s_exp_global.tsv \
+                -trr ../data/train_r.tsv -ot ../outputs/test_r_clu_dyn_a08_gd%s_gi%s_p.tsv \
+                -otr ../outputs/train_r_clu_dyn_a08_gd%s_gi%s_p.tsv \
                 -an 0.8 -av 0.8 -gnd %s -gvd %s -gni %s -gvi %s -tn 86400 \
-                > ../logs/uw-clu_dyn_a08_gd%s_gi%s_exp_global.log' % (gamma_decrease_name, gamma_increase_name, gamma_decrease_name, gamma_increase_name, gamma_decrease, gamma_decrease, gamma_increase, gamma_increase, gamma_decrease_name, gamma_increase_name)
+                > ../logs/uw-clu_dyn_a08_gd%s_gi%s_p_feature.log' % (gamma_decrease_name, gamma_increase_name, gamma_decrease_name, gamma_increase_name, gamma_decrease, gamma_decrease, gamma_increase, gamma_increase, gamma_decrease_name, gamma_increase_name)
       print cmd
       os.system(cmd)
 
