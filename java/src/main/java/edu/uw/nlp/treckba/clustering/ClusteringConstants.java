62b12b5a194aa930cf748c6b9906541ea313211e
nacho cano <nachoacano@gmail.com>
2014-09-10 03:19:58 -0700

    updating embeddings dimension

--- java/src/main/java/edu/uw/nlp/treckba/clustering/ClusteringConstants.java
+++ java/src/main/java/edu/uw/nlp/treckba/clustering/ClusteringConstants.java
@@ -4,7 +4,7 @@ public class ClusteringConstants {
 
 	public static final int NR_FEATURES = 25;
 	public static final int EMBEDDING_DIM = 300;
-	public static final int D = 629;
+	public static final int D = 929;
 	public static final float START_TIMELINESS = 0.5f;
 	public static final String WHITE_SPACE = " ";
 
