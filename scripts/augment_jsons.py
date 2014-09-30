4ef67621acfe1858f47c79990bf3693287d12e55
nacho cano <nachoacano@gmail.com>
2014-09-30 02:18:01 -0700

    adding python script that augments the json output

---
+++ scripts/augment_jsons.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import argparse
+import time
+from collections import defaultdict
+from os import listdir
+from os.path import isfile, join
+from os import rename
+import json
+
+
+def main():
+  parser = argparse.ArgumentParser(description='TODO')
+  parser.add_argument('-r', '--run_file', required=True)
+  parser.add_argument('-json', '--json_folder', required=True)
+  args = parser.parse_args()
+
+  run = defaultdict(defaultdict)
+  with open(args.run_file) as f:
+    for line in f.read().splitlines():
+      l = line.strip()
+      if l.startswith('#'):
+          continue
+      l = l.split('\t')
+      streamid = l[2]
+      entity = l[3][l[3].rfind('/')+1:]
+      score = int(l[4])
+      relevance = int(l[5])
+      run[entity][streamid] = (relevance, score)
+
+  onlyfiles = [ join(args.json_folder,f) for f in listdir(args.json_folder) if isfile(join(args.json_folder,f)) ]
+  
+  for filename in onlyfiles:
+    with open(filename, 'r') as f:
+      entity = filename[filename.rfind('/')+1:filename.rfind('.')]
+      json_file = json.load(f)
+      for d in json_file:
+        d['score'] = run[entity][d['streamid']][1]
+        if d['relevance'] == -10:
+          d['relevance'] = run[entity][d['streamid']][0]
+          break
+
+    tmp_file = filename + '.partial'
+    with open(tmp_file, 'w') as f:
+      f.write(json.dumps(json_file))
+    rename(tmp_file, filename)
+    break
+
+  
+if __name__ == '__main__':
+  main()
+
