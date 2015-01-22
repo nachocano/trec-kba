3bbde54301b35d22b919ed648d75a6ea1142266a
nachocano <nachoacano@gmail.com>
2015-01-22 01:37:18 -0800

    changing script

--- scripts/tfidf.py
+++ scripts/tfidf.py
@@ -16,11 +16,28 @@ def main():
   args = parser.parse_args()
 
   corpus = []
-  to_tfidf(corpus, args.train_bow_file, args.train_tfidf)
-  to_tfidf(corpus, args.test_bow_file, args.test_tfidf)
+  train_docs = []
+  test_docs = []
+  read_corpus(corpus, args.train_bow_file, train_docs)
+  read_corpus(corpus, args.test_bow_file, test_docs)
 
-def to_tfidf(corpus, input_file, output_file):
+  tfidf = tfidfmodel.TfidfModel(corpus)
+
+  to_tfidf(corpus, args.train_tfidf, train_docs)
+  to_tfidf(corpus, args.test_tfidf, test_docs)
+
+def to_tfidf(corpus, output_file, docs):
   of = open(output_file, 'w')
+  for doc in docs:
+    doc_tfidf = tfidf[doc]
+    doc_tfidf_as_arr = []
+    for w, v in doc_tfidf:
+      doc_tfidf_as_arr.append('%s,%s' % (w,v))
+    doc_tfidf_as_str = ' '.join(doc_tfidf_as_arr)
+    of.write('%s %s\n' % (fixed, doc_tfidf_as_str))
+  of.close()
+
+def read_corpus(corpus, input_file, docs):
   for line in open(input_file).read().splitlines():
     tokens = line.split()
     fixed = ' '.join(tokens[:29])
@@ -30,17 +47,9 @@ def to_tfidf(corpus, input_file, output_file):
       w, wc = bow.split(',')
       tup = (int(w), float(wc))
       doc.append(tup)
+    docs.append(doc)
     corpus.append(doc)
 
-    tfidf = tfidfmodel.TfidfModel(corpus)
-    doc_tfidf = tfidf[doc]
-    doc_tfidf_as_arr = []
-    for w, v in doc_tfidf:
-      doc_tfidf_as_arr.append('%s,%s' % (w,v))
-    doc_tfidf_as_str = ' '.join(doc_tfidf_as_arr)
-    of.write('%s %s\n' % (fixed, doc_tfidf_as_str))
-  of.close()
-      
 if __name__ == '__main__':
   main()
 
