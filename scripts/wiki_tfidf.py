46f344c61192c82c8d7130461bc7c7ec1ce5f479
Ignacio Agustin Cano <icano@bicycle.cs.washington.edu>
2015-01-25 19:17:08 -0800

    adding tfidf model save, should take 50 min according to radim, $#@$@

--- scripts/wiki_tfidf.py
+++ scripts/wiki_tfidf.py
@@ -1,7 +1,12 @@
-import gensim
+import gensim, bz2
 import argparse
 import time
 import sys
+
+def log(msg):
+  sys.stdout.write('%s\n' % msg)
+  sys.stdout.flush()
+
 
 def main():
   parser = argparse.ArgumentParser(description=__doc__)
@@ -10,16 +15,22 @@ def main():
   parser.add_argument('-te', '--test_bow_file', required=True)
   parser.add_argument('-otr', '--train_tfidf', required=True)
   parser.add_argument('-ote', '--test_tfidf', required=True)
-  parser.add_argument('-m', '--tfidf_model', required=True)
+  parser.add_argument('-c', '--corpus_file', required=True)
+  parser.add_argument('-m', '--model_output', required=True)
   args = parser.parse_args()
 
-
-  model = gensim.models.tfidfmodel.TfidfModel()
-  model.load(args.tfidf_model)
-
+  id2word = gensim.corpora.Dictionary.load_from_text(args.dictionary_file)
+  corpus_bow = gensim.corpora.MmCorpus(args.corpus_file)
+  log('loading model')
+  start = time.time()
+  model = gensim.models.tfidfmodel.TfidfModel(corpus=corpus_bow, id2word=id2word)
+  elapsed = time.time() - start
+  log('saving model, load took %s' % elapsed)
+  model.save(args.model_output)
+  log('to tfidf')
   to_tfidf(args.train_bow_file, args.train_tfidf, model)
   to_tfidf(args.test_bow_file, args.test_tfidf, model)
-
+  
 
 def to_tfidf(input_file, output_file, tfidf):
   i = 0 
